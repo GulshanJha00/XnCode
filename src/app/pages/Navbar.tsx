181b5348893fbd99f0110fd1fc8ebf23e1ca7332
Gulshan Kumar <151436211+GulshanJha00@users.noreply.github.com>
2024-12-30 21:40:12 +0530

    Update Navbar.tsx

--- src/app/pages/Navbar.tsx
+++ src/app/pages/Navbar.tsx
@@ -1,15 +1,18 @@
 import Button from "@/components/Button"
 import "./Navbar.css"
+import "Link" from next/line
 const Navbar = () => {
   return (
     <div className='navbg sticky h-20 w-full z-50 top-0 flex justify-between items-center'  
     >
+      <Link href="/">
       <h1 className=" text-2xl lg:text-5xl pl-10 lg:pl-[100px] font-serif"
          style={{
           fontFamily: "Itim",
           fontWeight: 700,
          }}
       >XnCode</h1>
+        </Link>
 
 <div className="flex gap-4 sm:gap-7 items-center pr-10 lg:pr-[100px]">
   <Button
