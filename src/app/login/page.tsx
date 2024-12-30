0b58f0d085b0a60e0dba47564e21109686f64d6d
Pranay Prasoon <159932934+pprasoon1@users.noreply.github.com>
2024-12-30 21:34:24 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
