fdf9f83b32239c5515e6e074df8e3c8966a9fc3f
GulshanJha00 <gk4763549@gmail.com>
2025-01-01 02:03:57 +0530

    right side /dsa  done

--- src/app/dsa/page.tsx
+++ src/app/dsa/page.tsx
@@ -29,73 +29,76 @@ const Page = () => {
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
   return (
+    <>
+    <div className="h-[755px] w-4 bg-blue-700 absolute left-[627px]"></div>
+   
     <div className="flex bg-[#1A1919] w-full h-max">
-      <div className="w-1/2 h-screen bg-white">
-        <h1 className="text-black">Yo</h1>
-      </div>
-
-      <div>
-        <div className="w-full bg-[#1A1919] border-2 border-[#312f2f] flex justify-between items-center h-20 px-4 py-4">
-          <div
-            className="flex cursor-pointer justify-center items-center p-3 py-2 gap-1 rounded-lg bg-[#745cee]"
-            onClick={handleGoBack}
-          >
-            <IoArrowBack className="text-[#1cbd1c] h-5 w-5 font-bold" />
-            <h1
-              className="font-semibold"
-              style={{
-                fontFamily: "Itim",
-                fontWeight: 700,
-              }}
-            >
-              Go Back
-            </h1>
-          </div>
-
-          <div className="flex  justify-center items-center gap-1">
-            <div className="flex justify-center bg-slate-300 p-3 py-1 gap-2 rounded-lg text-black items-center bg-">
-              <LuAlarmClock />
-              <div
-                className={` font-bold ${
-                  time <= 10000
-                    ? "text-red-500 animate-pulse"
-                    : "text-green-700"
-                }`}
-              >
-                {formatTime(time)}
-              </div>
-            </div>
-
-            <div className="flex cursor-pointer justify-center items-center bg-slate-300 text-black p-3 py-1 gap-1 rounded-lg">
-              <FaPlay />
-              <h1>Run</h1>
-            </div>
-          </div>
-
-          <div
-            className="flex cursor-pointer justify-center items-center p-3 py-2  gap-1 rounded-lg bg-[#1cbd1c] "
-            onClick={handleGoBack}
-          >
-            <RxUpload className=" h-5 w-5 font-bold" />
-            <h1
-              className="font-semibold "
-              style={{
-                fontFamily: "Itim",
-                fontWeight: 700,
-              }}
-            >
-              Submit
-            </h1>
-          </div>
+        <div className="w-1/2 h-[600px] bg-white">
+          <h1 className="text-black">Yo</h1>
         </div>
 
+      <div className="">
+          <div className="w-full bg-[#1A1919] border-x-4 border-b-4 border-t-8 border-blue-700 flex justify-between items-center h-20 px-4 py-4">
+              <div
+                className="flex cursor-pointer justify-center items-center p-3 py-2 gap-1 rounded-lg bg-[#745cee]"
+                onClick={handleGoBack}
+              >
+                <IoArrowBack className="text-[#1cbd1c] h-5 w-5 font-bold" />
+                <h1
+                  className="font-semibold"
+                  style={{
+                    fontFamily: "Itim",
+                    fontWeight: 700,
+                  }}
+                >
+                  Go Back
+                </h1>
+              </div>
+
+              <div className="flex  justify-center items-center gap-1">
+                <div className="flex justify-center bg-slate-300 p-3 py-1 gap-2 rounded-lg text-black items-center bg-">
+                  <LuAlarmClock />
+                  <div
+                    className={` font-bold ${
+                      time <= 10000
+                        ? "text-red-500 animate-pulse"
+                        : "text-green-700"
+                    }`}
+                  >
+                    {formatTime(time)}
+                  </div>
+                </div>
+
+                <div className="flex cursor-pointer justify-center items-center bg-slate-300 text-black p-3 py-1 gap-1 rounded-lg">
+                  <FaPlay />
+                  <h1>Run</h1>
+                </div>
+              </div>
+
+              <div
+                className="flex cursor-pointer justify-center items-center p-3 py-2  gap-1 rounded-lg bg-[#1cbd1c] "
+                onClick={handleGoBack}
+              >
+                <RxUpload className=" h-5 w-5 font-bold" />
+                <h1
+                  className="font-semibold "
+                  style={{
+                    fontFamily: "Itim",
+                    fontWeight: 700,
+                  }}
+                >
+                  Submit
+                </h1>
+              </div>
+          </div>
+
         <Editor
-          height="40vh"
+          height="50vh"
           width="100vh"
           defaultLanguage="cpp"
           defaultValue="//XnCode Is Ready To Serve You Master"
           theme="vs-dark"
-          className=" pt-3 pb-3 pl-4 pr-4 border-2 border-[#312f2f] rounded-lg shadow-lg mx-auto"
+          className=" pt-3 pb-3 pl-4 pr-4 border-l-8 border-b-8 border-x-4 border-blue-700 shadow-lg mx-auto"
           options={{
             fontSize: 16, // Adjust font size for better readability
             fontFamily: "'Source Code Pro', monospace",
@@ -108,8 +111,16 @@ const Page = () => {
             smoothScrolling: true, 
           }}
         />
+        <div className="pl-4 h-64 border-b-8 border-x-4 border-blue-700  ">
+          <h1>Test Cases</h1>
+        </div>
+
       </div>
+
+
+      
     </div>
+    </>
   );
 };
 
