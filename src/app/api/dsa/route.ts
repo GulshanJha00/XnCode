bf59974762bdf0c495f071b0c76d2ed2f46bbe35
GulshanJha00 <gk4763549@gmail.com>
2025-01-07 06:01:32 +0530

    folder added socket (work pending)

--- src/app/api/dsa/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const data = { value: 10 }; // Your desired value or logic
-  return NextResponse.json(data, { status: 200 });
-}
